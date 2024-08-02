833e45a811c45140cc26589dfda5412ce9934852
Diaz-Adrianz <diazadrianz191205@gmail.com>
2024-08-02 21:21:18 +0700

    fix: wali tugas ui

--- src/pages/wali/tugas.vue
+++ src/pages/wali/tugas.vue
@@ -147,6 +147,7 @@
                         <td>{{ getDateTime(item?.createdAt) }}</td>
                         <td>{{ item?.subject.name }}</td>
                         <td>{{ item?.topic }}</td>
+                        <td>{{ item?.description ?? "" }}</td>
                         <td>{{ getDateTime(item?.start_date) }}</td>
                         <td>{{ getDateTime(item?.end_date) }}</td>
                         <td>{{ item?.status }}</td>
@@ -193,7 +194,9 @@
                         </td>
                         <td class="text-center">{{ item?.subject.name }}</td>
                         <td class="text-center">{{ item?.topic }}</td>
-                        <td class="text-center">{{ item?.description }}</td>
+                        <td class="text-center">
+                          {{ item?.description ?? "-" }}
+                        </td>
 
                         <td class="text-center">
                           {{ getDateTime(item?.start_date) }}
@@ -221,6 +224,7 @@
                         <td>{{ getDateTime(item?.createdAt) }}</td>
                         <td>{{ item?.subject.name }}</td>
                         <td>{{ item?.topic }}</td>
+                        <td>{{ item?.description }}</td>
                         <td>{{ getDateTime(item?.start_date) }}</td>
                         <td>{{ getDateTime(item?.end_date) }}</td>
                         <td>{{ item?.status }}</td>
